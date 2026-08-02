f57149887b55103c709ae53e9f28123d652c6250
sanzid943 <sanzidmostofa@gmail.com>
2026-08-02 23:29:36 +0600

    updated the link

--- src/App.jsx
+++ src/App.jsx
@@ -392,7 +392,7 @@ export default function Portfolio() {
                 <Github size={15} /> view_projects()
               </a>
               <a
-                href="https://drive.google.com/file/d/1iytsZze-1RL0oajqau_4I9vRBusZcQTS/view?usp=drive_link"
+                href="https://drive.google.com/file/d/1ur-eiFEhAEoHPTu1E2c4DEiMa9y_koyw/view?usp=drive_link"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="btn-primary px-5 py-2.5 rounded-md text-sm font-semibold flex items-center gap-2 border"
